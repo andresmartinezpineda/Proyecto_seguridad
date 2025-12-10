b9a569ddc53e1d6c76711900a4b80dcd4a93f013
Andres Martinez <andrespineda_0@outlook.com>
2025-12-10 09:48:42 -0500

    primer estilo interfaz_main

--- interfaz_main.py
+++ interfaz_main.py
@@ -5,42 +5,69 @@ from interfaz_actualizar_meses import open_update_months
 
 root = tk.Tk()
 root.title("Panel Principal")
-root.geometry("800x600")
-root.configure(bg="white")
+root.geometry("600x450")
+root.configure(bg="#F5F5F5")   # gris claro moderno
 
-frame = tk.Frame(root, bg="white")
+frame = tk.Frame(root, bg="#F5F5F5")
+def on_enter(e):
+    e.widget['bg'] = "#005A9E"   # azul más oscuro
+
+def on_leave(e):
+    e.widget['bg'] = "#0072CE"
 frame.pack(expand=True)
+
+title_label = tk.Label(
+    frame,
+    text="PANEL PRINCIPAL",
+    bg="#F5F5F5",
+    fg="#0072CE",
+    font=("Segoe UI", 25, "bold")
+)
+title_label.pack(pady=(30, 10))
+
 
 button_style = {
     "width": 30,
-    "height": 3,
-    "bg": "#C0BFBF",
-    "fg": "black",
-    "borderwidth": 2,
-    "relief": "solid",
-    "font": ("Arial", 14, "bold"),
+    "height": 2,
+    "bg": "#0072CE",        # Azul Open English
+    "fg": "white",
+    "activebackground": "#005A9E",  # tono más oscuro al pulsar
+    "activeforeground": "white",
+    "borderwidth": 0,       # Quita bordes para look moderno
+    "relief": "flat",
+    "font": ("Segoe UI", 14, "bold"),  # fuente más moderna
+    "cursor": "hand2"       # mano al pasar
 }
 
-tk.Button(
+btn_upload = tk.Button(
     frame,
     text="SUBIR ORDENES",
     command=lambda: (open_upload_orders(root), root.withdraw()),
     **button_style
-).pack(pady=25)
+)
+btn_upload.pack(pady=15)
+btn_upload.bind("<Enter>", on_enter)
+btn_upload.bind("<Leave>", on_leave)
 
-tk.Button(
+btn_upload = tk.Button(
     frame,
     text="CREAR VENDOR",
     command=lambda: (create_vendor_window(root), root.withdraw()),
     **button_style
-).pack(pady=25)
+)
+btn_upload.pack(pady=15)
+btn_upload.bind("<Enter>", on_enter)
+btn_upload.bind("<Leave>", on_leave)
 
-tk.Button(
+btn_upload = tk.Button(
     frame,
     text="ACTUALIZAR MESES",
     command=lambda: (open_update_months(root), root.withdraw()),
     **button_style
-).pack(pady=25)
+)
+btn_upload.pack(pady=15)
+btn_upload.bind("<Enter>", on_enter)
+btn_upload.bind("<Leave>", on_leave)
 
 
 root.mainloop()
